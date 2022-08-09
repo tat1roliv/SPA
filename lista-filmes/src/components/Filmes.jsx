851500b6818d789ae42fe360cdeb55c4a64d7be0
tati oliveira <tat1roliv@gmail.com>
2022-08-09 20:46:27 -0300

    update

--- lista-filmes/src/components/Filmes.jsx
+++ lista-filmes/src/components/Filmes.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Filme from './Filme';
+
 const filmesEstilo = {
 border: '#F78B1F solid 2px', borderRadius: '25px', backgroundColor: '#E6E6E6', textAlign: 'Left',
 width: '500px', margin: 'auto',
 };
 
-function Filmes() { return (
-<div style={filmesEstilo}>
-<Filme />
-</div>
+function Filmes() { 
+    return (
+        <div style={filmesEstilo}>
+        <Filme />
+        </div>
 );
 }
 export default Filmes;
--- lista-filmes/src/components/Header.jsx
+++ lista-filmes/src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div>
             <h1>Star Wars: filmes de acordo com a data de lançamento</h1>
+            <h2>filmes data de lançamento</h2>
         </div>
     );
 }
