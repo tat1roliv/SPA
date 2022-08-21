825bfa8cb2817705ea4aacd4138c73db91caa43a
tati oliveira <tat1roliv@gmail.com>
2022-08-21 16:23:22 -0300

    components

--- app-origamid/src/App.js
+++ app-origamid/src/App.js
@@ -3,14 +3,19 @@ import ListaExibe  from './components/ListaExibe';
 import AppExDois from './components/AppExDois';
 import Evento from './components/Evento';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
 
   return (
     <div>
+      <Header />
       <AppExUm />
       <ListaExibe />
       <AppExDois />
       <Evento />
+      <Footer />
     </div>
   );
 }
