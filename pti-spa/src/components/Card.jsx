5ec06097f45de84c7265fbf6df526071bd5edbf8
tati oliveira <tat1roliv@gmail.com>
2022-08-28 21:22:46 -0300

    upload

--- pti-spa/src/components/Card.jsx
+++ pti-spa/src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Card.css'
 
 const Card = ({ image, title, description, url}) => {
     
   return ( 
-    <div className="card ">
+    <div className="card bg-light rounded-4">
         <img src={image} className="card-img-top rounded-4" alt="alt-image"/>
         <h5 className="card-title pt-3 px-3">{title}</h5>
         <p className="card-text py-1 px-3">{description}</p>
-        <a href={url} className="link pb-4 px-3 text-bold">Read more</a>
+        <a href={url} className="pb-4 px-3">Read more 	→</a>
     </div>
   );
 } 
