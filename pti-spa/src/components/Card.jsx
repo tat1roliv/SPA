520dbc750cf6242abb42f2ddbba20932c95245c7
tati oliveira <tat1roliv@gmail.com>
2022-08-28 21:14:18 -0300

    working

--- pti-spa/src/components/Card.jsx
+++ pti-spa/src/components/Card.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
-const Card = () => {
-
-    const dados = {
-
-    };
+const Card = ({ image, title, description, url}) => {
     
   return ( 
-    <div className="card">
-        <img src="#" className="card-img-top" alt="alt-image"/>
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="card ">
+        <img src={image} className="card-img-top rounded-4" alt="alt-image"/>
+        <h5 className="card-title pt-3 px-3">{title}</h5>
+        <p className="card-text py-1 px-3">{description}</p>
+        <a href={url} className="link pb-4 px-3 text-bold">Read more</a>
     </div>
   );
 } 
--- pti-spa/src/components/CardDeck.jsx
+++ pti-spa/src/components/CardDeck.jsx
@@ -1,18 +1,53 @@
 import React from 'react';
 import Card from './Card.jsx';
 
+import image1 from '../assets/pexels-photo-1.jpeg';
+import image2 from '../assets/pexels-photo-2.jpeg';
+import image3 from '../assets/pexels-photo-3.jpeg';
+
 const CardDeck = () => {
+
+    const cards = [
+        {
+            id: 1,
+            image: image1,
+            title: 'New seafood recipe perfect for losing weight.',
+            description: 'Mass comunication strategies to continue focusing on brand awareness.',
+            url: 'https://tatioliveira.tech/',
+        },
+        {
+            id: 2,
+            image: image2,
+            title: 'Balance os proteins, fats and carbo. How do I get it?',
+            description: 'Mass comunication strategies to continue focusing on brand awareness.',
+            url: 'https://tatioliveira.tech/',
+        },
+        {
+            id: 3,
+            image: image3,
+            title: 'The importance of proper and healthy eating.',
+            description: 'Mass comunication strategies to continue focusing on brand awareness.',
+            url: 'https://tatioliveira.tech/',
+        },
+
+    ]
   return ( 
     <div className="row flex-column flex-lg-row justify-content-center align-items-center">
-        <div className="col-12 col-lg-4">
-            <Card />
-        </div> 
-        <div className="col-12 col-lg-4">
-            <Card />
-        </div> 
-        <div className="col-12 col-lg-4">
-            <Card />
-        </div> 
+        
+        {
+            cards.map( (card) => (
+                <div className="col-12 col-lg-4">
+                    <Card 
+                        key={card.id} 
+                        title={card.title} 
+                        description={card.description}
+                        url={card.url}
+                        image={card.image}
+                        />
+                </div> 
+            ))
+        }
+
     </div>
   );
 } 
