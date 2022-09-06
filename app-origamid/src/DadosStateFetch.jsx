9b4e51d7223a4eea83bbd8f149647b5b4a7593b8
tati oliveira <tat1roliv@gmail.com>
2022-09-06 19:23:31 -0300

    consumo api

--- app-origamid/src/DadosStateFetch.jsx
+++ app-origamid/src/DadosStateFetch.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { useState } from 'react';
+import Produto from './components/Produto'
 
 const DadosStateFetch = () => {
 
     const [dados, setDados] = useState(null);
+    const [loading, setLoading] = useState(null);
 
     async function handleClick(event){
+
+        setLoading(true);
+
         const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
         );
-        console.log(response);
+
+        const json = await response.json();
+        setDados(json);
+
+        setLoading(false);
     }
 
   return (
     <div>
     
-        <button onClick={handleClick}>notebook</button>
-        <button onClick={handleClick}>smartphone</button>
-        <button onClick={handleClick}>tablet</button>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>notebook</button>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>smartphone</button>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>tablet</button>
+
+        { loading && <p>Carregando...</p>}
+
+        { !loading && dados ? <Produto dados={dados}/> : 'sem produtos para exibir' }
 
     </div>
   )
--- pti-spa/src/App.js
+++ pti-spa/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import SectionTitles from './components/SectionTitles';
 import CardDeck from './components/CardDeck'
 
-
-function App() {
-
-  
+function App() {  
   return (
     <div className="container">
       <SectionTitles key={"sectionTitles"} />
