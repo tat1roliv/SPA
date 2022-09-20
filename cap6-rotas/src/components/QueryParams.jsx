0b32f72370157567777cfe0b69053c600b6fe2f9
tati oliveira <tat1roliv@gmail.com>
2022-09-20 19:43:14 -0300

    vite project

--- cap6-rotas/src/components/QueryParams.jsx
+++ cap6-rotas/src/components/QueryParams.jsx
@@ -2,14 +2,14 @@ import { Link, useLocation } from 'react-router-dom';
  import ChildComponent from './ChildComponent';
 
 const QueryParams = () => {
-let query = new URLSearchParams(useLocation(). search);
+let query = new URLSearchParams(useLocation().search);
 return (
 <div className="container">
 <h1>Requisições de query</h1>
 <div className="showcase">
 <ul>
 <li>
-<Link to="/ teste?name=Kylo&surname=Ren">Kylo Ren</Link>
+<Link to="/teste?name=Kylo&surname=Ren">Kylo Ren</Link>
 </li>
 <li>
 <Link to="/teste?name=Rey">Rey</Link>
