e760d84c467c1a525b8238dff71a71982d1508bb
tati oliveira <tat1roliv@gmail.com>
2022-09-20 14:40:24 -0300

    rotas

--- basic-redux/src/App.js
+++ basic-redux/src/App.js
@@ -1,12 +1,13 @@
 import { Provider } from 'react-redux'; 
 import store from './store';
-
+import Contador from './components/Contador'
 function App() { 
   return (
     <Provider store={store}>
       <div>
         <header>
           <h1>Contador de cliques</h1>
+          <Contador />
         </header>
       </div>
     </Provider>
