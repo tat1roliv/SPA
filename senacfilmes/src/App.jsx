d3d27dfda637259e2a021f6365e4f3543702c1f2
tati oliveira <tat1roliv@gmail.com>
2022-09-21 11:50:23 -0300

    web 5 e 6 cards map

--- senacfilmes/src/App.jsx
+++ senacfilmes/src/App.jsx
@@ -3,29 +3,30 @@ import CardFilme from './components/CardFilme'
 
 function App() {
 
-  const sandman = {
-    poster_path: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/q54qEgagGOYCq5D1903eBVMNkbo.jpg',
-    title:'Sandman',
-    score: 99,
-  }
 
-  const stranger = {
-    poster_path: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5xiUqrCFgT6wU2KzynZMu5CXkM6.jpg',
-    title:'Stranger Things',
-    score: 98,
-  }
-
-  const dark = {
-    poster_path: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jrxOAXQDIKAhEYtcbD7fpTbnhHM.jpg',
-    title:'Dark',
-    score: 97,
-  }
+  const dados = [
+    {
+      poster_path: 'q54qEgagGOYCq5D1903eBVMNkbo.jpg',
+      title:'Sandman',
+      score: 99,
+    },
+    {
+      poster_path: '5xiUqrCFgT6wU2KzynZMu5CXkM6.jpg',
+      title:'Stranger Things',
+      score: 98,
+    },
+    {
+      poster_path: 'jrxOAXQDIKAhEYtcbD7fpTbnhHM.jpg',
+      title:'Dark',
+      score: 97,
+    }
+  ]
 
   return (
     <section className="container">
-      <CardFilme filme={sandman} />
-      <CardFilme filme={dark} />
-      <CardFilme filme={stranger} />
+
+      {dados.map( (filme) => <CardFilme filme={filme} />)}
+      
     </section>  
   )
 }
--- senacfilmes/src/components/CardFilme.jsx
+++ senacfilmes/src/components/CardFilme.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './CardFilme.css'
 
-const CardFilme = (props) => {
+const CardFilme = ({filme}) => {
   return (
     <div className="card">
-      <img src={props.filme.poster_path} alt="test"/>
-      <p>{props.filme.title}</p>
-      <span>✨ {props.filme.score}</span>
+      <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"+ filme.poster_path} alt="test"/>
+      <p>{filme.title}</p>
+      <span>✨ {filme.score}</span>
       <a href="#">Detalhes</a>
 
     </div>
