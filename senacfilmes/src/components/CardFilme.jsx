4e785fce9258fefb6978a61fa78b6734aae431b0
tati oliveira <tat1roliv@gmail.com>
2022-09-21 13:05:03 -0300

    web 5 e 6

--- senacfilmes/src/components/CardFilme.jsx
+++ senacfilmes/src/components/CardFilme.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import './CardFilme.css'
+import './CardFilme.css';
+import { useState} from 'react';
+
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const CardFilme = ({filme}) => {
+
+  const [favorite, setFavorite] = useState(false);
+
+  function toogleFavorite() {
+    setFavorite(!favorite);
+  }
+
   return (
     <div className="card">
+      {favorite ? 
+        <FavoriteIcon onClick={toogleFavorite} className="icon-favorite"/> 
+        : 
+        <FavoriteBorderOutlinedIcon onClick={toogleFavorite} className="icon-favorite"/>
+      }
       <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"+ filme.poster_path} alt="test"/>
       <p>{filme.title}</p>
       <span>✨ {filme.score}</span>
